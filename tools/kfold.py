feab67627be153e9830ab700f13643d8ab463778
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2022-12-03 13:29:48 +0700

    update: import activation functions

--- tools/kfold.py
+++ tools/kfold.py
@@ -19,7 +19,7 @@ from core.utils import set_gpu_limit, load_data, save_dump, write_score, get_cal
 from core.model import model_classification
 from core.model_hsc_v1 import created_model_hsc_01
 # from core.model_segReLU import model_classification_segReLU
-from core.utils_activation import activation_functions
+
 
 
 # Parse command line arguments
@@ -81,9 +81,8 @@ metrics = [
 
 print("loading model .....")
 dict_model = {
-    "model-base": model_classification(input_layer=ip_shape, num_class=num_classes, activation_block=activation_functions[activation_block], activation_dense='softmax'),
-    "hsc-v1": created_model_hsc_01(input_shape=ip_shape, number_class=num_classes, activation_block=activation_functions[activation_block], activation_dense='softmax'),
-    "model-segReLU": model_classification(input_layer=ip_shape, num_class=num_classes, activation_block=activation_functions[activation_block], activation_dense='softmax')
+    "model-base": model_classification(input_layer=ip_shape, num_class=num_classes, activation_block=activation_block, activation_dense='softmax'),
+    "hsc-v1": created_model_hsc_01(input_shape=ip_shape, number_class=num_classes, activation_block=activation_block, activation_dense='softmax')
 }
 
 model = dict_model[mode_model]
